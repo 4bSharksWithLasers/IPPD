f2aaff33ad3b7157500cb6eb71228e371877b62a
walkharddotninja <smith.phillip.m@live.com>
2016-02-04 13:19:29 -0500

    Adjusted routing for current pages. Added dummy admin page. Altered file structure slightly.

--- Resources/server.js
+++ Resources/server.js
@@ -1,17 +1,31 @@
-var config = require("./config");
+var config = require("./Assets/Config/config");
 var express = require("express");
 var path = require("path");
 
 var server = express();
 var port = config.port;
 
+// open port
 server.listen(port);
 console.log("Listening: Port", port);
 
-//server.set('/', path.join(__dirname, '/'));
+// set routes to static assets (images and stylesheets)
 server.use('/Styles', express.static(__dirname + '/Styles'));
 server.use('/Assets', express.static(__dirname + '/Assets'));
+
 // set routes
+
+// index.html
 server.get('/', function(req, res) {
-  	res.sendFile(__dirname + '/Views/index.html');
+  res.sendFile(__dirname + '/Views/index.html');
+});
+
+// registration.html
+server.get('/registration', function(req, res) {
+	res.sendFile(__dirname + '/Views/registration.html');
+});
+
+// admin.html
+server.get('/admin', function(req, res) {
+	res.sendFile(__dirname + '/Views/admin.html');
 });
