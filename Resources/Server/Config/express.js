2310f091e3150bff04e237feb09efb2bd46cd142
Amy <amy.devries@ufl.edu>
2016-02-14 11:57:56 -0500

    Added code to express and routes files
    Wrote code, based off of bootcamp assignments for express and routing
    files. Not quite sure about what routing needs to take place or if the
    code works.

--- Resources/Server/Config/express.js
+++ Resources/Server/Config/express.js
@@ -1,1 +1,35 @@
+var path = require('path'), 
+	express = require('express'), 
+	mongoose = require('mongoose'),
+	morgan = require('morgan'),
+	bodyParser = require('body-parser'),
+	config = require('../../../config/env/development.js'),
+	reviewRouter = require('../Routes/review.server.routes.js'); 
 
+module.exports.init = function(){
+	//connect to database
+	mongoose.connect(config.db.uri);
+
+	//initialize app
+	var app = express(); 
+
+	//enable request logging for development debugging
+	app.user(morgan('dev'));
+
+	//body parsing middleware
+	app.use(bodyParser.json());
+
+	//serve static files
+	app.use('/', express.static(_dirname + '/../../client'));
+
+	//user the review router for request to the api
+	//NOT SURE ABOUT THE BELOW LINE. not sure about the routing, where things should go
+	app.use('/api/review', reviewRouter);
+
+	//go to homepage for all routes not specified
+	app.all('/*', function(req, res){
+		res.sendFile(path.resolve('Client/index.html'));
+	});
+
+	return app; 
+};
