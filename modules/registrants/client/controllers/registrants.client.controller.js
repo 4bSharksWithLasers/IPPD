2a20692967db3dc1c5dbc851a02ed61da4d900d9
Amy <amy.devries@ufl.edu>
2016-02-24 21:17:34 -0500

    Fixing stuff that got messed up from MergingMaster branch

--- modules/registrants/client/controllers/registrants.client.controller.js
+++ modules/registrants/client/controllers/registrants.client.controller.js
@@ -1,9 +1,13 @@
 'use strict';
 
 // Registrants controller
-angular.module('registrants').controller('RegistrantsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Registrants', 'Teams',
-  function ($scope, $stateParams, $location, Authentication, Registrants, Teams) {
+angular.module('registrants').controller('RegistrantsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Registrants', 'Teams', 'Affiliations',
+  function ($scope, $stateParams, $location, Authentication, Registrants, Teams, Affiliations) {
     $scope.authentication = Authentication;
+
+    //Pull the list of teams and affiliations from DB
+    $scope.teamDropdowns = Teams.query();
+    $scope.affiliationDropdowns = Affiliations.query();
 
     // Create new Registrant
     $scope.create = function (isValid) {
