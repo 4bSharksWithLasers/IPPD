eb172eba2baff5272cb63b9b901ab8138f81f8fd
justintime5046 <justintime5046@gmail.com>
2016-03-19 20:35:16 -0400

    fixed ngStorage Error where no pages where loading

--- config/assets/default.js
+++ config/assets/default.js
@@ -17,7 +17,6 @@ module.exports = {
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
-        'public/lib/ngstorage/ngStorage.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
--- config/assets/production.js
+++ config/assets/production.js
@@ -17,7 +17,6 @@ module.exports = {
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
         'public/lib/angular-file-upload/angular-file-upload.min.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
-        'public/lib/ngstorage/ngStorage.min.js'
       ]
     },
     css: 'public/dist/application.min.css',
