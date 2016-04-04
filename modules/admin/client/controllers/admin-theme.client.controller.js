3cedc8879adba10047bff7c69f3d52024ad58dd1
Steven <sstevenwwilliams@aol.com>
2016-04-04 12:38:26 -0400

    admin logo preview

--- modules/admin/client/controllers/admin-theme.client.controller.js
+++ modules/admin/client/controllers/admin-theme.client.controller.js
@@ -6,16 +6,16 @@ angular.module('admin').controller('ThemeController', function($scope) {
   $scope.changeColor = function () {
     $scope.myStyle={ 'background-color': '#' + $scope.colorChoice };
   };
- $scope.setFile = function(element) {
-  $scope.currentFile = element.files[0];
-   var reader = new FileReader();
+  $scope.setFile = function(element) {
+    $scope.currentFile = element.files[0];
+    var reader = new FileReader();
 
-  reader.onload = function(event) {
-    $scope.image_source = event.target.result
-    $scope.$apply()
+    reader.onload = function(event) {
+      $scope.image_source = event.target.result;
+      $scope.$apply();
 
-  }
+    };
   // when the file is read it triggers the onload event above.
-  reader.readAsDataURL(element.files[0]);
-} 
+    reader.readAsDataURL(element.files[0]);
+  };
 });
